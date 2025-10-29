05c44139ef87eaa2dc8e8db2de96e7fca7f31ec8
josephdattilo03 <jadattilobusiness@gmail.com>
2025-10-29 17:40:20 -0400

    got local setup to launch local api

--- backend/planwise-api/functions/events/create_event.py
+++ backend/planwise-api/functions/events/create_event.py
@@ -1,3 +1,23 @@
+from shared.models.event import Event
+from shared.utils.db import get_table
+import json
+from pydantic import ValidationError
 
 def lambda_handler(event, context):
-    pass
+    try:
+        table = get_table("events-table")
+        body = json.loads(event['body'])
+        
+        event_data = Event(**body)
+        item = event_data.model_dump(mode='json')
+        table.put_item(Item=item)
+        
+        return {
+            'statusCode': 200,
+            'body': json.dumps({
+                'message': 'Event created successfully',
+                'event_id': event_data.id
+            })
+        }
+    except ValidationError as e:
+        return {'statusCode': 400, 'body': json.dumps({'error': e.errors()})}
--- backend/planwise-api/layers/dependencies/python/shared/models/event.py
+++ backend/planwise-api/layers/dependencies/python/shared/models/event.py
@@ -1,5 +1,5 @@
 # Please use pydantic model validation
-from pydantic import BaseModel
+from pydantic import BaseModel, field_serializer
 from datetime import date
 from typing import Literal, List, Optional
 
@@ -8,6 +8,12 @@ class Recurrence(BaseModel):
     day_of_week : List[Literal["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"]]
     date_until : date
     date_start : Optional[date] = None
+
+    @field_serializer('date_until', 'date_start')
+    def serialize_date(self, value: date, _info):
+        if value is None:
+            return None
+        return value.isoformat()
 
 class Event(BaseModel):
     id : int
@@ -20,3 +26,7 @@ class Event(BaseModel):
     location: str
     timezone: str
     recurrence : Optional[Recurrence]
+
+    @field_serializer('start_time', 'end_time')
+    def serialize_date(self, value: date, _info):
+        return value.isoformat()
